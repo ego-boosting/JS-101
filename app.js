387fef2d9485b1d4f881085b7ff0ebc8d632e757
EC2 Default User <kazukipump7@gmail.com>
2022-10-20 11:17:39 +0000

    [JS]a+b+c

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-for (let i = 1; i <= 50; i++ ) {
-  if(i % 15 === 0) {
-    console.log("FizzBuzz")
-  } else if(i % 5 === 0) {
-    console.log("Fizz")
-  } else if(i % 3 === 0) {
-    console.log("Buzz") 
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if(i % 5 === 0 && i % 3 === 0 ) {
+//     console.log("JavaScript")
+//   } else if(i % 5 === 0 ) {
+//     console.log("Java")
+//   } else if(i % 3 === 0 ) {
+//     console.log("Script")
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+const a = "楽しい"
+const b = "ruby"
+const c = "をする"
+
+console.log(a+b+c)
