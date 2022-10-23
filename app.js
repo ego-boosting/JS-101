3fc46642414f7800d5fef74f0ba5a46b34b855cd
EC2 Default User <kazukipump7@gmail.com>
2022-10-23 08:46:09 +0000

    [Js]fizzbuzz

--- app.js
+++ app.js
@@ -10,9 +10,15 @@ for (let i = 1; i <= 100; i++) {
   }
 }
 
-for (let i = 1; i <= 100; i++) {
-  if(i % 5 === 0 && i % 3 === 0) {
-    
+for (let i = 1; i <= 25; i++) {
+  if(i % 4 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz")
+  } else if(i % 4 === 0) {
+    console.log("Fizz")
+  } else if(i % 3 === 0) {
+    console.log("Buzz")
+  } else {
+    console.log(i)
   }
 }
 
